6af71ef4ba2aa8436c336222165a2c9b2fcd8d89
NSEsell <89134171+NSEsell@users.noreply.github.com>
2021-08-19 12:38:13 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -14,8 +14,36 @@
 #     print("yes")
 # else:
 #     print("no")
-str = input()
-if (int(str[0])+int(str[1])+int(str[2]) )== (int(str[3]) + int(str[4]) + int(str[5])):
-    print("Счастливый")
-else:
-    print("Обычный")
+# str = input()
+# if (int(str[0])+int(str[1])+int(str[2]) )== (int(str[3]) + int(str[4]) + int(str[5])):
+#     print("Счастливый")
+# else:
+#     print("Обычный")
+# s = int(input())
+# b = 0
+# for i in range (s):
+#     a = int(input())
+#     b = a + b
+# print(b)
+# a, b, c = map(int, input().split())
+# print(a,b,c)
+# i = 0
+# str = input()
+# while str[ i ] != '0':
+#      i = i + 1
+# print( i )
+# i = input()
+# planets = ['Earth','Venus','Saturn','Mars']
+# for i, el in enumerate (planets):
+#     print(i, el)
+# i = input()
+# str = 'abcdefghijk'
+# for i, el in enumerate (str) :
+#     if i % 3 == 0:
+#         print(i, el)
+# fh=open("hello.txt", "w")
+# fh.write("Hello World\n")
+# fh.write("Hello\n")
+# fh.close()
+# fh = open ("hello.txt","r")
+# print(fh.read())
