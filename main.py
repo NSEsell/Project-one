edc69916ec10dbc1a7b4f961d84ff61b182130c2
NSEsell <89134171+NSEsell@users.noreply.github.com>
2021-08-19 10:23:29 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#a = int(input())
+#b = int(input())
+#s=input()
+#if s=='+':
+#    print(a+b)
+#elif s == '-':
+#    print(a-b)
+#elif s == '*':
+#    print(a*b)
+#else:
+#   print("Некоректный ответ")
+# str = input()
+# if str[0] and len(str) > 10:
+#     print("yes")
+# else:
+#     print("no")
+str = input()
+if (int(str[0])+int(str[1])+int(str[2]) )== (int(str[3]) + int(str[4]) + int(str[5])):
+    print("Счастливый")
+else:
+    print("Обычный")
